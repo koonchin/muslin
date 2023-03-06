e96bf544518223dbdbfac9482135561266e0cbfe
= <=>
2023-03-06 15:40:11 +0700

    06032023 add test.txt

--- source.py
+++ source.py
@@ -1,8 +1,7 @@
 # access mysql
 import os
 from pathlib import Path
-
-hostdb = '139.162.37.6'
+hostdb = '139.162.28.194'
 
 passworddb = 'Chino002'
 
@@ -14,4 +13,6 @@ host = '127.0.0.1:8000'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR = os.path.join(BASE_DIR,'app')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
---
+++ webhook/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+# URLConf
+urlpatterns = [
+    path('callback/',views.callback),
+    path('update/',views.editorder),
+    path('updatetracking/',views.updatetracking),
+    path('add/',views.addorder),
+    path('editproduct/',views.editproduct),
+    path('update_maruay/',views.editorder_maruay),
+    path('updatetracking_maruay/',views.updatetracking_maruay),
+    path('add_maruay/',views.addorder_maruay),
+    path('editproduct_maruay/',views.editproduct_maruay),
+]
